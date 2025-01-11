04e627298fbe92b9e5159513ee391df794fba33b
EIlif Johansen <eilifjohansen@outlook.com>
2025-01-12 00:19:42 +0100

    Forenklet og fornorsket

--- app/components/crud/read.tsx
+++ app/components/crud/read.tsx
@@ -35,8 +35,8 @@ export default function GetApps({ apps, error }: { apps: App[], error: string | 
                                             <div
                                                 className="bg-white shadow-lg rounded-lg p-6 w-full transform transition-transform hover:scale-105 hover:bg-gray-100 h-full flex flex-col">
                                                 <h2 className="text-2xl font-semibold mb-2">{app.app_name}</h2>
-                                                <p className="text-gray-700">Active: {app.is_active ? 'Yes' : 'No'}</p>
-                                                <p className="text-gray-700">Created at: {new Date(app.created_at).toLocaleDateString()}</p>
+                                                {/*<p className="text-gray-700">Aktiv: {app.is_active ? 'Ja' : 'Nei'}</p>*/}
+                                                <p className="text-gray-700">Opprettet: {new Date(app.created_at).toLocaleDateString()}</p>
                                             </div>
                                         </a>
                                     ))}
