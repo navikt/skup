deef81463c9a02283d5e3794fe19b527e84ac563
EIlif Johansen <eilifjohansen@outlook.com>
2025-01-11 22:47:00 +0100

    La til mer logging

--- app/api/read/route.ts
+++ app/api/read/route.ts
@@ -10,17 +10,20 @@ export async function GET() {
         });
 
         if (!response.ok) {
-            throw new Error('Network response was not ok');
+            const errorDetails = await response.text();
+            console.error('Network response was not ok:', response.status, errorDetails);
+            throw new Error(`Network response was not ok: ${response.status} - ${errorDetails}`);
         }
 
         const data = await response.json();
         return NextResponse.json(data);
     } catch (error) {
         if (error instanceof Error) {
+            console.error('Fetch failed:', error.message);
             return NextResponse.json({ error: error.message }, { status: 500 });
         } else {
+            console.error('An unknown error occurred');
             return NextResponse.json({ error: 'An unknown error occurred' }, { status: 500 });
         }
     }
 }
-
