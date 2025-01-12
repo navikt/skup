0f292954cccd7997e6690fe5fb4f1d0afe618543
EIlif Johansen <eilifjohansen@outlook.com>
2025-01-12 21:32:05 +0100

    Sorter på dato

--- app/components/crud/read.tsx
+++ app/components/crud/read.tsx
@@ -17,6 +17,8 @@ export default function GetApps({ apps, error }: { apps: App[], error: string | 
         }
     }, [apps, error]);
 
+    const sortedApps = [...apps].sort((b, a) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime());
+
     return (
         <>
             <h1 className="text-4xl font-bold mb-8">Apper (bare demo greier)</h1>
@@ -26,11 +28,11 @@ export default function GetApps({ apps, error }: { apps: App[], error: string | 
                         <div>Uff da: {error}</div>
                     ) : (
                         <>
-                            {(!apps || apps.length === 0) ? (
+                            {(!sortedApps || sortedApps.length === 0) ? (
                                 <div>Fant ingen apper</div>
                             ) : (
                                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-                                    {apps.map((app) => (
+                                    {sortedApps.map((app) => (
                                         <a key={app.app_id} href="#" className="block w-full">
                                             <div
                                                 className="bg-white shadow-lg rounded-lg p-6 w-full transform transition-transform hover:scale-105 hover:bg-gray-100 h-full flex flex-col">
