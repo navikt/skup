de153f470a504e21e0e966836b9c109d62776454
EIlif Johansen <eilifjohansen@outlook.com>
2025-01-12 16:46:20 +0100

    La til mer logging

--- app/api/read/route.ts
+++ app/api/read/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
+    console.log('Starting GET request to fetch apps');
     try {
         const response = await fetch('https://skupapi.intern.nav.no/api/apps', {
             method: 'GET',
@@ -16,11 +17,12 @@ export async function GET() {
         }
 
         const data = await response.json();
+        console.log('Successfully fetched apps:', data);
         return NextResponse.json(data);
     } catch (error) {
         if (error instanceof Error) {
             console.error('Fetch failed:', error.message, error.stack);
-            return NextResponse.json({ error: error.message, stack: error.stack }, { status: 500 });
+            return NextResponse.json({ error: 'Fetch failed', message: error.message, stack: error.stack }, { status: 500 });
         } else {
             console.error('An unknown error occurred');
             return NextResponse.json({ error: 'An unknown error occurred' }, { status: 500 });
